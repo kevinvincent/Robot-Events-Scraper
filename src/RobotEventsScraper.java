8c52f25b2855c7d4b7479c91b128ed284323944c
kevinvincent <kevinsundar@gmail.com>
2013-11-20 10:09:00 -0800

    I commit to commit

--- src/RobotEventsScraper.java
+++ src/RobotEventsScraper.java
@@ -2,6 +2,7 @@
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 
 import org.jsoup.*;
@@ -10,20 +11,33 @@ import org.jsoup.nodes.*;
 import org.jsoup.select.*;
 
 class Match {
-	public String name;
+	
+	public int div;
+	public String type;
+	public String id;
 	public String time;
 	public String redA;
 	public String redB;
+	public String redC;
 	public String blueA;
 	public String blueB;
+	public String blueC;
 	
-	public Match(String name, String time, String redA, String redB, Striing blueA, String blueB) {
-		this.name = name;
+	public Match(int div, String type, String id, String time, String redA, String redB, String redC, String blueA, String blueB, String blueC) {
+		this.div = div;
+		this.type = type;
+		this.id = id;
 		this.time = time;
 		this.redA = redA;
 		this.redB = redB;
+		this.redC = redC;
 		this.blueA = blueA;
 		this.blueB = blueB;
+		this.blueC = blueC;
+	}
+	
+	public String toString() {
+		return id + " - " +time + " - " +redA + " - " +redB + " - " +redC + " - " +blueA + " - " +blueB + " - " +blueC;
 	}
 }
 
@@ -61,15 +75,32 @@ class Event {
 				 for (Element row : table.select("tr:gt(0)")) eventDetails.add(row.text());
 		
 		//Get Matches
+				 System.out.println("Getting matches..");
 				 for(int i = 1; i <= numberOfDivisions; i++) {
-					Elements matchbits = doc.select("div[class=matchbit]");
-					for(Element matchbit : matchbits) {
-						int numberOfColumns = matchbit.select("div").size();
-						if( numberOfColumns == 10) { 
-							System.out.println(matchbit.select("div").first().text());
+					 	System.out.println("Getting matches for division " + i);
+					 	doc = Jsoup.connect("http://ajax.robotevents.com/tm/results/matches/?format=html&div="+i+"&sku="+sku).userAgent("Mozilla").timeout(20*1000).get();
+						Elements matchbits = doc.select("div[class=matchbit]");
+						
+						for(Element matchbit : matchbits) {
+							if(matchbit.select("div[class^=matchcol]").size() > 9) {
+									
+								String[] matchTitle = matchbit.select("div[class^=matchcol]").get(0).text().split(" ");
+								String redA = matchbit.select("div[class^=matchcol]").get(1).text();
+								String redB = matchbit.select("div[class^=matchcol]").get(2).text();
+								String redC = matchbit.select("div[class^=matchcol]").get(3).text();
+								String blueA = matchbit.select("div[class^=matchcol]").get(4).text();
+								String blueB = matchbit.select("div[class^=matchcol]").get(5).text();
+								String blueC = matchbit.select("div[class^=matchcol]").get(6).text();
+								
+								if(matchTitle.length==3) {
+									eventMatches.add(new Match(i,matchTitle[0],matchTitle[1],matchTitle[2],redA,redB,redC,blueA,blueB,blueC));
+								}
+								else {
+									eventMatches.add(new Match(i,matchTitle[0],matchTitle[1],null,redA,redB,redC,blueA,blueB,blueC));
+								}
+							}
 						}
-					}
-				 }
+					 }
 	}
 	
 	public String toString() {
@@ -77,21 +108,14 @@ class Event {
 	}
 }
 
-/* ------------------------------------ END MAIN -------------------------------------- */
+/* ------------------------------------ Start MAIN -------------------------------------- */
 public class RobotEventsScraper {
 	
 	public static void main(String args[]) throws IOException {
-		Document doc = Jsoup.connect("http://www.robotevents.com/2013-vex-robotics-high-school-world-championship.html").userAgent("Mozilla").timeout(20*1000).get();
-		int numberOfDivisions = doc.select("div[id^=division]").size()-1;
-		for(int i = 1; i <= numberOfDivisions; i++) {
-			Elements matchbits = doc.select("div[class=matchbit]");
-			for(Element matchbit : matchbits) {
-				int numberOfColumns = matchbit.select("div").size();
-				if( numberOfColumns == 10) { 
-					System.out.println(matchbit.select("div").first().text());
-				}
-			}
-		 }
+		ArrayList<Event> events = ScraperUtils.getEventList();
+		events.get(100).url="http://www.robotevents.com/2013-vex-robotics-high-school-world-championship.html";
+		events.get(100).reloadEventDetails();
+		System.out.println(events.get(100).eventMatches.get(2).toString());
 	}
 }
 
